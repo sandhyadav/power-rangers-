b41674506fa895531b410ca6de31f59786aea247
sandhyadav <66108338+sandhyadav@users.noreply.github.com>
2023-04-24 18:39:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// Create a variable canvas
+
+block_y=1;
+block_x=1;
+
+player_x=10
+player_y=10
+
+block_image_width = 350;
+block_image_height = 480;
+
+var block_image_object= "";
+
+// Complete the function new_image() to add new image
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+	// Use appropriate keycode to add red image
+	
+	
+	// Use appropriate keycode to add green image
+	
+	// Use appropriate keycode to add yellow image
+	
+	// Use appropriate keycode to add pink image
+	
+	// Use appropriate keycode to add blue image
+	
+}
+
